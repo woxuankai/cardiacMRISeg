5045edecf817307e5c558b7ce91549367006b6a0
Kyle Xuan <woxuankai@gmail.com>
2017-03-05 15:42:24 +0800

    fixed bug in run.sh

--- multiatlas/run.sh
+++ multiatlas/run.sh
@@ -6,9 +6,9 @@ SPEC='d0'
 VOL='v1'
 LN='4'
 JOBS='4'
-test $# -le 2 && { SPEC="$1"; VOL=$2; }
-test $# -le 3 && LN="$3"
-test $# -le 4 && JOBS="$4"
+test $# -ge 2 && { SPEC="$1"; VOL=$2; }
+test $# -ge 3 && LN="$3"
+test $# -ge 4 && JOBS="$4"
 
 
 TARGET="${SPEC}_${VOL}"
@@ -29,9 +29,10 @@ echo "altases argument ${ARG}"
 ./multi_atlas_seg.sh \
 	-t "./data/${TARGET}.nii.gz" \
 	-p "${PREDICTION}" \
-	-v \
+	 \
 	-j ${JOBS} \
 	-n ${LN} \
 	${ARG}
 
-LabelOverlapMeasures 2 "${PREDICTION}" "./data/${TARGET}_seg.nii.gz"
+
+LabelOverlapMeasures 2 "${PREDICTION}" "./data/${TARGET}_seg.nii.gz" | cut -d$'\n' -f6 | tr -s ' ' | cut -d' ' -f5 | echo "dice: $( cat - )"
