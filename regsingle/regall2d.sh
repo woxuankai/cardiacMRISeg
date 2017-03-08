cc3bcb00d6cdd0ab85dfb83b1aa4958ba0700231
Kyle Xuan <woxuankai@gmail.com>
2017-03-08 20:35:23 +0800

    nothing serious

--- regsingle/regall2d.sh
+++ regsingle/regall2d.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 OPTS='-d'
 REG='./doreg.sh'
-$REG d0 d1 2 $OPTS 2>/dev/null | grep -i dice &
-$REG d0 d2 2 $OPTS 2>/dev/null | grep -i dice &
-$REG d1 d0 2 $OPTS 2>/dev/null | grep -i dice &
-$REG d1 d2 2 $OPTS 2>/dev/null | grep -i dice &
-$REG d2 d0 2 $OPTS 2>/dev/null | grep -i dice &
-$REG d2 d1 2 $OPTS 2>/dev/null | grep -i dice &
+
+#$REG d0 d1 2 $OPTS 2>/dev/null | grep -i dice &
+#$REG d0 d2 2 $OPTS 2>/dev/null | grep -i dice &
+#$REG d1 d0 2 $OPTS 2>/dev/null | grep -i dice &
+#$REG d1 d2 2 $OPTS 2>/dev/null | grep -i dice &
+#$REG d2 d0 2 $OPTS 2>/dev/null | grep -i dice &
+#$REG d2 d1 2 $OPTS 2>/dev/null | grep -i dice &
+
+
+$REG d7 d12 2 $OPTS 2>/dev/null | grep -i dice &
+$REG d8 d12 2 $OPTS 2>/dev/null | grep -i dice &
+$REG d12 d7 2 $OPTS 2>/dev/null | grep -i dice &
+$REG d12 d8 2 $OPTS 2>/dev/null | grep -i dice &
+$REG d7 d8 2 $OPTS 2>/dev/null | grep -i dice &
+$REG d8 d7 2 $OPTS 2>/dev/null | grep -i dice &
 wait
 exit 0
