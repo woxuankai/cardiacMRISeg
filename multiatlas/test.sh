535d0b40c84432d46bb83ba35fcdeaac734ae1c5
Kyle Xuan <woxuankai@gmail.com>
2017-04-17 09:48:47 +0800

    changed test.sh

--- multiatlas/test.sh
+++ multiatlas/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in 0 1 2
+for i in 0
 do
-	for j in 0 5 10
+	for j in 0 2 4 6 8 10 12 14 16 18
 	do
-		for k in 2 4 6
+		for k in 8 7
 		do
 			./run.sh -d${i} -v${j} -s${k} -j4 -n5 -m1
 		done
