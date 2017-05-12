7b53cbfbd3bbe4b723dcbf12a0fbe770c628a055
Kyle Xuan <woxuankai@gmail.com>
2017-05-12 15:31:12 +0800

    use MI metric only

--- multiatlas/doreg.sh
+++ multiatlas/doreg.sh
@@ -109,14 +109,19 @@ antsRegistration \
 	--interpolation Linear --use-histogram-matching 0 \
 	--winsorize-image-intensities '[0.005,0.995]' \
 	--initial-moving-transform "[$FIXED,$MOVING,1]" \
+  --transform 'Rigid[0.1]' \
+	--metric "MI[$FIXED,$MOVING,1,32]" \
 	--transform 'Affine[0.1]' \
-	--metric "MI[$FIXED,$MOVING,1,24]" \
-	--convergence [1000x500x250x0,1e-6,10] \
+	--convergence [1000x500x250x100,1e-6,10] \
+	--shrink-factors 8x4x2x1 \
+	--smoothing-sigmas 3x2x1x0vox \
+	--metric "MI[$FIXED,$MOVING,1,32]" \
+	--convergence [1000x500x250x100,1e-6,10] \
 	--shrink-factors 8x4x2x1 \
 	--smoothing-sigmas 3x2x1x0vox \
 	--transform BSpline[0.1,200] \
-	--metric "CC[$FIXED,$MOVING,1,4]" \
-	--convergence [800x400x200x0,1e-6,10] \
+	--metric "MI[$FIXED,$MOVING,1,32]" \
+	--convergence [1000x500x250x100,1e-6,10] \
 	--shrink-factors 8x4x2x1 \
 	--smoothing-sigmas 3x2x1x0vox \
 	|| { echo "Error!! failed to do registration" >&2; exit 1;}
