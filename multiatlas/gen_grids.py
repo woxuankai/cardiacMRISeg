0ab32e362eb3e94c8b4d06cb138b484ea6c216ac
Kyle Xuan <woxuankai@gmail.com>
2017-05-26 09:03:50 +0800

    use nifti1 instead of nifti2 in gen_grids.py

--- multiatlas/gen_grids.py
+++ multiatlas/gen_grids.py
@@ -7,6 +7,22 @@ import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 
-if len(os.argv) != 3:
-    print('usage: '+os.argv[0]+'input.nii.gz output.nii.gz')
+if len(sys.argv) != 3:
+    print('usage: '+sys.argv[0]+' input.nii.gz output.nii.gz')
+    sys.exit()
 
+img=nib.load(sys.argv[1])
+data=img.get_data()
+affine=img.get_affine()
+shape=img.get_shape()
+
+for x in range(0, shape[0]):
+    for y in range(0, shape[1]):
+        if x%5 == 3 or y%5 == 3:
+            data[x,y]=1
+        else:
+            data[x,y]=0
+
+new_img=nib.Nifti1Image(data,affine)
+#new_img.header=img.get_header()
+nib.save(new_img,sys.argv[2])
